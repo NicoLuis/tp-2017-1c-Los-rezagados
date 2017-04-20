8654b01b7699d86dbd37fe90306060a31853e1dd
NicoLuis <nicolasaluis@hotmail.com>
2017-04-20 20:29:04 -0300

    agrego fd select solo si es consola

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -152,19 +152,22 @@ int conectar_select(char* puerto_escucha) {
 		descriptoresLectura = fdsMaestro;
 
 		if (select(max_fds + 1, &descriptoresLectura, NULL, NULL, NULL) == -1) {
-			if(errno==EINTR) select(max_fds + 1, &descriptoresLectura, NULL, NULL, NULL);
-			else	log_error(conectar_select_log, "Error en el select");
+			if(errno==EINTR)
+				select(max_fds + 1, &descriptoresLectura, NULL, NULL, NULL);
+			else
+				log_error(conectar_select_log, "Error en el select");
 		}
 
 		for (current_fd  = 0; current_fd  <= max_fds; current_fd++) {
 			if (FD_ISSET(current_fd , &descriptoresLectura)) {
 				if (current_fd  == servidor) {
+
 					if ((socket_cliente_aceptado = aceptarCliente(servidor)) == -1){
 						log_error(conectar_select_log, "Error al aceptar conexion");
 						perror("Error al aceptar conexion");
 					} else {
 // Aca entro cuando entro una nueva conexion		-----------------------------------------------------------------
-						FD_SET(socket_cliente_aceptado, &fdsMaestro);
+
 						if (socket_cliente_aceptado > max_fds) max_fds = socket_cliente_aceptado;
 						log_trace(conectar_select_log, "El select recibio una conexion en el socket %d", socket_cliente_aceptado);
 
@@ -175,13 +178,14 @@ int conectar_select(char* puerto_escucha) {
 
 							case 1:
 // Aca entro cuando entro una nueva consola		-----------------------------------------------------------------
+								FD_SET(socket_cliente_aceptado, &fdsMaestro);
 								list_add(lista_consolas, &socket_cliente_aceptado);
 
 								break;
 							case 2:
 // Aca entro cuando entro una nueva cpu			-----------------------------------------------------------------
 
-								if (pthread_create(&hilo_conexionCPU, &atributo,(void*) escucharCPU, (void*) socket_cliente_aceptado) < 0) {	//fixme: el hilo para CPU queda?
+								if (pthread_create(&hilo_conexionCPU, &atributo,(void*) escucharCPU, (void*) socket_cliente_aceptado) < 0) {
 									log_error(conectar_select_log, "Error Hilo Escucha Consola");
 									abort();
 								}
@@ -200,9 +204,8 @@ int conectar_select(char* puerto_escucha) {
 
 					}
 
-				} else {
+				} else
 					atender_consola(current_fd);		//todo: ver q pasa con las conexiones de CPU
-				}
 			}
 		}
 	}

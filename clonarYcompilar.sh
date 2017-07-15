1762d7faaee52ffb14cea91413c4ec8c4889fb67
buccellato <francobuccellato@gmail.com>
2017-07-14 23:15:11 -0300

    Archivos de config

---
+++ clonarYcompilar.sh
@@ -0,0 +1,6 @@
+#!usr/bin/sh
+
+clear
+
+sudo sh clone.sh
+sudo sh compilar.sh
---
+++ clone.sh
@@ -0,0 +1,7 @@
+#!usr/bin/sh
+
+cd ..
+
+git clone https://github.com/sisoputnfrba/so-commons-library
+git clone https://github.com/sisoputnfrba/ansisop-parser
+

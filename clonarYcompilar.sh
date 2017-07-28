419b8e954f29ad2c4f650dce8ac78f619e98c03e
emilalli <emilianogomezlalli@gmail.com>
2017-07-28 12:21:36 -0300

    Borrando clonar y compilar

--- clonarYcompilar.sh
+++
@@ -1,6 +0,0 @@
-#!usr/bin/sh
-
-clear
-
-sudo sh clone.sh
-sudo sh compilar.sh
--- clone.sh
+++
@@ -1,7 +0,0 @@
-#!usr/bin/sh
-
-cd ..
-
-git clone https://github.com/sisoputnfrba/so-commons-library
-git clone https://github.com/sisoputnfrba/ansisop-parser
-
